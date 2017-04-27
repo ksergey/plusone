221ec2966ecc2fc7b3a682b45bc15b091b5f6f79
Sergey Kovalevich <inndie@gmail.com>
2017-04-27 23:35:52 +0300

    new sample

---
+++ examples/dispatch/exception.hpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017 Sergey Kovalevich <inndie@gmail.com>
+ */
+
+#ifndef KSERGEY_exception_270417232830
+#define KSERGEY_exception_270417232830
+
+#include <plusone/exception.hpp>
+
+namespace dispatch {
+
+using error = plusone::tagged_exception< struct dispatch >;
+
+} /* namespace dispatch */
+
+#endif /* KSERGEY_exception_270417232830 */
---
+++ examples/dispatch/main.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Sergey Kovalevich <inndie@gmail.com>
+ */
+
+#include <netinet/ip.h>
+#include <cstdlib>
+#include <csignal>
+#include <iostream>
+#include <plusone/net/mmap_rx.hpp>
+#include "multicast_channel.hpp"
+
+static sig_atomic_t sigint = 0;
+
+static void sighandler(int num)
+{ sigint = 1; }
+
+int main(int argc, char* argv[])
+{
+    char* iface = nullptr;
+    if (argc == 2) {
+        iface = argv[1];
+    }
+
+    try {
+        signal(SIGINT, sighandler);
+
+        if (iface) {
+            std::cout << "interface " << iface << '\n';
+        }
+
+        dispatch::multicast_channel channel;
+
+        while (__likely(!sigint)) {
+        }
+    } catch (const std::exception& e) {
+        std::cout << "Error: " << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
