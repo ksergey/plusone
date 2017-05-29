131973dec82aa757c4d08149652dbde00f163059
Sergey Kovalevich <inndie@gmail.com>
2017-05-29 16:41:48 +0300

    -event_loop

--- include/plusone/signal.hpp
+++ include/plusone/signal.hpp
@@ -5,12 +5,10 @@
 #ifndef MADLIFE_signal_101116205803_MADLIFE
 #define MADLIFE_signal_101116205803_MADLIFE
 
-#include <sys/signalfd.h>
 #include <csignal>
 #include <cstring>
 #include <functional>
 #include <initializer_list>
-#include "event_loop.hpp"
 
 namespace plusone {
 
@@ -22,58 +20,7 @@ private:
     /* Callback function */
     static callback_type global_callback_;
 
-    event_loop& loop_;
-    sigset_t old_mask_;
-    int fd_{-1};
-    callback_type callback_;
-    event_loop::handle handle_{nullptr};
-
 public:
-    /** Signal exception type */
-    using error = tagged_exception< signal >;
-
-    signal(const signal&) = delete;
-    signal& operator=(const signal&) = delete;
-
-    /** Construct signal listener */
-    template< class Callback >
-    signal(event_loop& loop, std::initializer_list< int > sigs, Callback&& callback)
-        : loop_{loop}
-        , callback_{callback}
-    {
-        sigset_t mask;
-        sigemptyset(&mask);
-        for (auto sig: sigs) {
-            sigaddset(&mask, sig);
-        }
-
-        sigprocmask(SIG_BLOCK, &mask, &old_mask_);
-
-        fd_ = signalfd(-1, &mask, 0);
-        if (__unlikely(fd_ == -1)) {
-            throw error{"Failed to create signalfd"};
-        }
-
-        /* Add handler to loop */
-        handle_ = loop_.add(fd_, EPOLLIN, [this](int descriptor, std::uint32_t events) {
-            __expect( events & EPOLLIN );
-            signalfd_siginfo data;
-            int rc = ::read(fd_, &data, sizeof(data));
-            if (__unlikely(rc != sizeof(data))) {
-                throw error{"Internal error (%s:%d)", __FILE__, __LINE__};
-            }
-            callback_(data.ssi_signo);
-        });
-    }
-
-    /** Destroy signal handler */
-    __force_inline ~signal() noexcept
-    {
-        loop_.remove(handle_);
-        ::close(fd_);
-        sigprocmask(SIG_SETMASK, &old_mask_, nullptr);
-    }
-
     /** Setup signal handler */
     template< class Callback >
     static __force_inline void setup(std::initializer_list< int > sigs, Callback&& callback)
