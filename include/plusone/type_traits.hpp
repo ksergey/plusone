b9e00f45890284e910ded718673037b6692ca694
Sergey Kovalevich <inndie@gmail.com>
2017-10-25 17:07:00 +0300

    Added for_each

--- include/plusone/type_traits.hpp
+++ include/plusone/type_traits.hpp
@@ -25,6 +25,28 @@ struct make_void
 template< class... Ts >
 using void_t = typename detail::make_void< Ts... >::type;
 
+/**
+ * Call F for each argument.
+ *
+ * Example:
+ * @code
+ *   int x = 1;
+ *   double y = 2;
+ *   std::string s = "hello";
+ *
+ *   for_each([](auto& s) {
+ *     std::cout << x << '\n';
+ *   }, x, y, s);
+ * @endcode
+ *
+ * @see https://stackoverflow.com/a/28279483
+ */
+template< class F, class... Args >
+void for_each(F&& f, Args&&... args)
+{
+    [](...){}((f(std::forward< Args >(args)), 0)...);
+}
+
 } /* namespace plusone */
 
 #endif /* KSERGEY_type_traits_241017182855 */
