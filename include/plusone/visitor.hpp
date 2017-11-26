14ea21fdd81e5d9b12c6f4d669fd9b09060f46fc
Sergey Kovalevich <inndie@gmail.com>
2017-11-26 14:25:08 +0300

    Added make_visitor

---
+++ include/plusone/visitor.hpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2017 Sergey Kovalevich <inndie@gmail.com>
+ */
+
+#ifndef KSERGEY_visitor_261117141401
+#define KSERGEY_visitor_261117141401
+
+#include <type_traits>
+
+namespace plusone {
+namespace detail {
+
+template< class... Fs >
+struct visitor_impl;
+
+template< class F, class... Fs >
+struct visitor_impl< F, Fs... >
+    : visitor_impl< Fs... >
+    , F
+{
+    using F::operator();
+    using visitor_impl< Fs... >::operator();
+
+    visitor_impl(F&& f, Fs&&... fs)
+        : visitor_impl< Fs... >{std::forward< Fs >(fs)...}
+        , F{std::forward< F >(f)}
+    {}
+};
+
+template< class F >
+struct visitor_impl< F >
+    : F
+{
+    using F::operator();
+
+    visitor_impl(F&& f)
+        : F{std::forward< F >(f)}
+    {}
+};
+
+} /* namespace detail */
+
+/**
+ * Make functional object from lambdas.
+ *
+ * @code
+ * auto visitor = make_visitor(
+ *    [](int v) { std::cout << v << '\n'; },
+ *    [](int x, int y) { std::cout << x + y << '\n'; },
+ *    [](const std::string& v) { std::cout << v << '\n'; }
+ * );
+ * ...
+ * visitor("test");
+ * visitor(1);
+ * visitor(3, 2);
+ * @endcode
+ */
+template< class... Fs >
+auto make_visitor(Fs&&... fs) noexcept
+{
+    using visitor_type = detail::visitor_impl< std::decay_t< Fs >... >;
+    return visitor_type{std::forward< Fs >(fs)...};
+}
+
+} /* namespace plusone */
+
+#endif /* KSERGEY_visitor_261117141401 */
--- tests/lambda.cpp
+++ tests/lambda.cpp
@@ -5,41 +5,7 @@
 #include <iostream>
 #include <string>
 #include <plusone/compiler.hpp>
-
-template< class... Ts >
-struct lambda_visitor;
-
-template< class T, class... Ts >
-struct lambda_visitor< T, Ts... >
-    : lambda_visitor< Ts... >
-    , T
-{
-    using T::operator();
-    using lambda_visitor< Ts... >::operator();
-
-    lambda_visitor(T t, Ts... ts)
-        : lambda_visitor< Ts... >(ts...)
-        , T{t}
-    {}
-};
-
-template< class T >
-struct lambda_visitor< T >
-    : T
-{
-    using T::operator();
-
-    lambda_visitor(T t)
-        : T{t}
-    {}
-};
-
-template< class... Fs >
-auto make_lambda(Fs&&... fs)
-{
-    using type = lambda_visitor< std::decay_t< Fs >... >;
-    return type{std::forward< Fs >(fs)...};
-}
+#include <plusone/visitor.hpp>
 
 int main(int argc, char* argv[])
 {
@@ -49,18 +15,23 @@ int main(int argc, char* argv[])
     std::string string_value{"Test"};
 
     try {
-        auto v = make_lambda(
+        auto v = plusone::make_visitor(
             [](int v) {
                 std::cout << "int value " << v << '\n';
             },
             [](const std::string& v) {
                 std::cout << "string value " << v << '\n';
+            },
+            [](int x, int y) {
+                std::cout << "x=" << x << ",y=" << y << "\n";
             }
         );
 
         v(string_value);
         v(int_value);
         v(7);
+        v("hello");
+        v(3, 2);
     } catch (const std::exception& e) {
         std::cout << "ERROR: " << e.what() << '\n';
         return EXIT_FAILURE;
